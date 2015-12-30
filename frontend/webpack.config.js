0ca5729cce36d3557cc453dea8d824a4d562da8e
Layl <layl@edgeworksgames.com>
2015-12-30 01:40:35 +0100

    Initial files

---
+++ frontend/webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        path: './target',
+        filename: 'index.js'
+    },
+
+    resolve: {
+        modulesDirectories: ['node_modules'],
+        extensions: ['', '.js', '.elm']
+    },
+
+    module: {
+        loaders: [{
+            test: /\.html$/,
+            exclude: [/node_modules/],
+            loader: 'file?name=[name].[ext]'
+        }, {
+            test: /\.elm$/,
+            exclude: [/elm-stuff/, /node_modules/],
+            loader: 'elm-webpack'
+        }],
+
+        noParse: [/.elm$/]
+    }
+};
